92a5ebb5c5bc4919362248b30ad5183774248b42
Obelianko <bianka77@inbox.ru>
2021-11-25 12:32:25 +0300

    allure set up readme update

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-[![Build status](https://ci.appveyor.com/api/projects/status/q1b3vcirq5gx2852?svg=true)](https://ci.appveyor.com/project/Obelianko/2-3-1-carddelivery-patterns)
+[![Build status](https://ci.appveyor.com/api/projects/status/wlod333plo9nlb6n?svg=true)](https://ci.appveyor.com/project/Obelianko/4-3-1-reportingallure)
+
 
 
--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -1,5 +1,9 @@
 package ru.netology.delivery.test;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -13,6 +17,16 @@ import static com.codeborne.selenide.Selenide.*;
 
 
 public class DeliveryTest {
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
     @BeforeEach
     public void setUp() {
         open("http://localhost:9999");
